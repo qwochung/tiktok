5704da43810bb1435633919ab38f46221b869bfe
qhung <tranquochung0404@gmail.com>
2025-02-21 20:20:45 +0700

    create layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+
+import style from './Header.module.scss';
+
+const cx = classNames.bind(style);
+
 function Header() {
-    return  <h2>Header</h2>;
+    return <h2 className={cx('wrapper')}>
+        <div className={cx('inner')}></div>
+    </h2>;
 }
 
 export default Header;
