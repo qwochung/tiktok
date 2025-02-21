c6a010e0002ef1dc50328c32659cca715863bb9c
qhung <tranquochung0404@gmail.com>
2025-02-21 09:57:48 +0700

    create layout

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,13 @@
+import Header from '~/components/Layout/components/Header';
+
+function Upload() {
+    return (
+        <div>
+            <div className="content">
+                <h2>Upload Pages</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Upload;
