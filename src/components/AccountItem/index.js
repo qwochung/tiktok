8542f3f32d4a610001ad8c288d661f08706c139b
qhung <tranquochung0404@gmail.com>
2025-02-26 22:35:05 +0700

    intergrate search API

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,27 +1,35 @@
 
 import classNames from "classnames/bind";
-import styles  from './AccountItem.module.scss'
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import Image from "../Image";
 
+
+import Image from "../Image";
+import styles  from './AccountItem.module.scss'
 
 const cx = classNames.bind(styles)
 
-function AccountItem() {
+function AccountItem( {data}) {
+
+    // console.log('data:', data );
+    
     return ( 
-        <div className={cx('wrappper')}>
-            <Image className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/ecc37f16c700bdbb667a66ec317fb8dc~tplv-tiktokx-cropcenter:100:100.jpeg?dr=14579&nonce=19361&refresh_token=a678c65a77f2667f2032878f46087083&x-expires=1740718800&x-signature=uWszrJKEysXfT%2BDqXfCXc%2FAsuSY%3D&idc=my&ps=13740610&shcp=81f88b70&shp=a5d48078&t=4d5b0474" alt="img"></Image>
+        <Link to={`/user/${data.nickname}`} className={cx('wrappper')}>
+            <Image className={cx('avatar')} 
+                    src={data.avatar}
+                    alt={data.nickname}>
+            </Image>
             <div className={cx('infor')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Van A</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                    <span>{data.full_name}</span>
+                    {data.tick &&  <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>}
                 </h4>
-                <span className={cx('userName')}>@avan</span>
+                <span className={cx('userName')}>@{data.nickname}</span>
             </div>
 
 
-        </div>
+        </Link>
      );
 }
 
