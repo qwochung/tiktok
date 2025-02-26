9dd8670acc932ebc5ed33f6f1602038c01002cea
qhung <tranquochung0404@gmail.com>
2025-02-26 14:55:53 +0700

    handle search function

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -3,6 +3,7 @@ import classNames from "classnames/bind";
 import styles  from './AccountItem.module.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import Image from "../Image";
 
 
 const cx = classNames.bind(styles)
@@ -10,7 +11,7 @@ const cx = classNames.bind(styles)
 function AccountItem() {
     return ( 
         <div className={cx('wrappper')}>
-            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/04c26c73206e8997c961c50194ee9ae5~tplv-tiktokx-cropcenter:100:100.jpeg?dr=14579&nonce=42422&refresh_token=90959b5e5aede7f122a6ce49c7660474&x-expires=1740546000&x-signature=OIDB37lie%2B3DQAw0hi7Upfz8svs%3D&idc=my&ps=13740610&shcp=c1333099&shp=30310797&t=4d5b0474" alt="img"></img>
+            <Image className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/ecc37f16c700bdbb667a66ec317fb8dc~tplv-tiktokx-cropcenter:100:100.jpeg?dr=14579&nonce=19361&refresh_token=a678c65a77f2667f2032878f46087083&x-expires=1740718800&x-signature=uWszrJKEysXfT%2BDqXfCXc%2FAsuSY%3D&idc=my&ps=13740610&shcp=81f88b70&shp=a5d48078&t=4d5b0474" alt="img"></Image>
             <div className={cx('infor')}>
                 <h4 className={cx('name')}>
                     <span>Nguyen Van A</span>
